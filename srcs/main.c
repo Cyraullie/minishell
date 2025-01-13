45422acadac06af0cfd2b4d0fc4d112d6c38f4b9
Loic Pittet <lpittet@c2r5s2.42lausanne.ch>
2025-01-13 16:11:46 +0100

    created modifiable env

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,41 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 13:35:36 by lpittet           #+#    #+#             */
-/*   Updated: 2025/01/13 14:09:27 by lpittet          ###   ########.fr       */
+/*   Updated: 2025/01/13 16:10:09 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main()
+char	**get_env(char **envp)
+{
+	int		i;
+	char	**env;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	env = malloc(sizeof(char *) * (i + 1));
+	if (!env)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		env[i] = envp[i];
+		i++;
+	}
+	return (env);
+}
+
+int main(int ac, char **av, char **envp)
 {
 	char	*line;
+	char	**env;
 	
+	env = av;     //TODO remove these lines placeholders to be able to compile
+	if (ac < 0)     
+		return (1);
+	env = get_env(envp);
 	while (1)
 	{
 		line = readline("minishell> ");
