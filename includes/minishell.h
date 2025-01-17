a8f115035bd1d384fc5ff930ac0de39b1452a8e5
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-17 16:21:45 +0100

    add needed include and prototype of signal functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 13:38:10 by lpittet           #+#    #+#             */
-/*   Updated: 2025/01/13 15:35:11 by lpittet          ###   ########.fr       */
+/*   Updated: 2025/01/17 16:13:32 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stdio.h>
 # include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
 # include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
@@ -24,12 +26,18 @@ typedef struct s_command
 	char	*cmd;
 	char	*flags;
 	char	**cmd_tab;
-} t_command;
+}		t_command;
 
 // parsing_utils.c
 char	**mini_split(char const *s, char c);
 
 // parsing.c
 int		parsing(char *line, t_command *cmd);
+
+
+// signal.c
+void	handle_sigint(int sig);
+void	init_sig(void);
+void	handle_eof(char *line);
 
 #endif
