190020a7ca77f24c9fe11f2fdf4caf8a20556cf6
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-17 16:27:51 +0100

    add get env

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,48 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 13:35:36 by lpittet           #+#    #+#             */
-/*   Updated: 2025/01/17 16:23:09 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/17 16:27:25 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(void)
+/**
+ * @brief create a modifiable environment variable
+ * @param envp , env variable
+ * @return NULL on failure, char **env on success
+ */
+char	**get_env(char **envp)
+{
+	int		i;
+	char	**env;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	env = malloc(sizeof(char *) * (i + 1));
+	if (!env)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		env[i] = envp[i];
+		i++;
+	}
+	env[i] = NULL;
+	return (env);
+}
+
+int	main(int ac, char **av, char **envp)
 {
 	char		*line;
+	char		**env;
 	//t_command	cmd;
 
+	(void)ac;
+	(void)av;
+	env = get_env(envp);
+	(void)env;
 	init_sig();
 	while (1)
 	{
