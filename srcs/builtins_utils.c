56d4d3acea243631821b048eb302dfc4495a88c0
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-20 16:08:12 +0100

    add utils to use builtin cd

--- srcs/builtins_utils.c
+++ srcs/builtins_utils.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 17:10:47 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/17 17:34:45 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/20 16:06:48 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,27 @@ int	find_valid_flag(char *msg, char flag)
 		return (1);
 	}
 	return (0);
+}
+
+char	*get_path(void)
+{
+	char	*p;
+
+	p = malloc(sizeof(char *) * BUFFER_SIZE);
+	getcwd(p, BUFFER_SIZE);
+	return (p);
+}
+
+char	*get_userhome(void)
+{
+	char	*p;
+	char	**p_a;
+	char	*u_p;
+
+	u_p = malloc(sizeof(char) * 6);
+	u_p = "/home/";
+	p = get_path();
+	p_a = ft_split(p, '/');
+	u_p = ft_strjoin(u_p, p_a[1]);
+	return (u_p);
 }
