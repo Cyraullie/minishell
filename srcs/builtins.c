82e9f70c75e860b3ca98f3320811163e5feb21a8
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-20 13:05:28 +0100

    add exit builtin and describe functions

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,17 +6,16 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:30:34 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/20 11:03:48 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/20 13:03:51 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
 /**
- * @brief 
+ * @brief function to write a text and handle -n option
  * 
- * @param msg 
- * @param n 
+ * @param cmd get command line array
  */
 void	echo(char **cmd)
 {
@@ -41,6 +40,37 @@ void	echo(char **cmd)
 }
 
 /**
+ * @brief function to exit the program
+ * 
+ * @param cmd get command line array
+ * @return int return error state
+ */
+int	ft_exit(char **cmd)
+{
+	int	i;
+
+	printf("exit\n");
+	i = 0;
+	while (cmd[1][i] != '\0')
+	{
+		if (!ft_isdigit(cmd[1][i]))
+		{
+			printf("bash: exit: %s: numeric argument required\n", cmd[1]);
+			exit(2);
+		}
+		i++;
+	}
+	if (cmd[2])
+	{
+		printf("bash: exit: too many arguments\n");
+		return (1);
+	}
+	if (cmd[1])
+		exit(ft_atoi(cmd[1]));
+	exit(0);
+}
+
+/**
  * @brief 
  * 
  * @param env 
@@ -49,4 +79,3 @@ void	echo(char **cmd)
 {
 
 }*/
-
