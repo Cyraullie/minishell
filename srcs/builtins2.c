8d62b982c4cf1d8dd748b12ddf38a7541d4388f1
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-21 17:24:25 +0100

    add builtins export with argument

--- srcs/builtins2.c
+++ srcs/builtins2.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 16:20:02 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/21 15:03:53 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/21 17:22:00 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,29 +45,37 @@ char	**ft_unset(char **cmd, char **env)
 	}
 	return (nenv);
 }
-//TODO export tout court = declare -x ZDOTDIR="/home/cgoldens"
+
+/**
+ * @brief function to add an environment variable
+ * 
+ * @param cmd get command line array
+ * @param env get environment var
+ * @return char** the state of the actual environment variable
+ */
 char	**ft_export(char **cmd, char **env)
 {
 	char	**nenv;
+	char	**name;
 	int		i;
 	int		j;
 
-	nenv = NULL;
 	j = 1;
-	if (!cmd[j])
+	if (!cmd[j])//TODO export tout court = declare -x ZDOTDIR="/home/cgoldens"
 		write_env(env);
 	while (cmd[j])
 	{
+		name = ft_split(cmd[j], '=');
 		i = 0;
 		while (env[i])
 			i++;
-		if (get_envline(env, cmd[i]) == -1)
-			nenv = ft_calloc(sizeof(char *), i + 1);
+		if (get_envline(env, name[0]) == -1)
+			nenv = ft_calloc(sizeof(char *), i + 2);
 		else
-			nenv = ft_calloc(sizeof(char *), i);
+			nenv = ft_calloc(sizeof(char *), i + 1);
 		if (!nenv)
 			return (NULL);
-		del_envline(env, nenv, cmd[j++]);
+		add_envline(env, nenv, cmd[j++], name);
 		clean_env(env);
 		env = nenv;
 	}
