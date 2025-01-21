92879e644c6fdba3b2bed6694b436bd1d11b509c
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-21 17:27:17 +0100

    add function to add new line in env or update it

--- srcs/builtins_utils2.c
+++ srcs/builtins_utils2.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:05:36 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/21 15:31:22 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/21 17:20:46 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,44 @@ void	write_env(char **env)
 	{
 		printf("declare -x %s\n", env[i++]);
 	}
+}
+
+/**
+ * @brief function to add a line in env var
+ * 
+ * @param env environment variable
+ * @param nenv new environment variable
+ * @param title name of the variable 
+ */
+void	add_envline(char **env, char **nenv, char *title, char **name)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (env[i])
+	{
+		if (i != get_envline(env, name[0]))
+			dup_env(env, nenv, i, j++);
+		else
+		{
+			nenv[j] = ft_strdup(title);
+			check_tabenv(nenv, j++);
+		}
+		i++;
+	}
+	if (get_envline(env, name[0]) == -1) 
+	{
+		nenv[j] = ft_strdup(title);
+		check_tabenv(nenv, j++);
+	}
+	nenv[j] = NULL;
+	clean_env(name);
+}
+
+void	dup_env(char **env, char **nenv, int i, int j)
+{
+	nenv[j] = ft_strdup(env[i]);
+	check_tabenv(nenv, j);
 }
