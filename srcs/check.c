4c0531dfb91b5bba6d18de714a105ca1f1466822
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-23 16:37:44 +0100

    add functions to check if env is sorted

--- srcs/check.c
+++ srcs/check.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 17:10:33 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/21 17:11:53 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/23 16:27:37 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,46 @@ void	check_tabenv(char **env, int i)
 		clean_env(env);
 		return ;
 	}
+}
+
+/**
+ * @brief check if all env tab is sorted
+ * 
+ * @param pos position for each element in env
+ * @param name name for each element in env
+ * @return int 0 if it isnt sorted 1 if it's sorted
+ */
+int	is_order(int *pos, char **name)
+{
+	int	i;
+
+	i = 0;
+	while (name[i])
+	{
+		if (pos[i] != -1)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * @brief Get the maxlength env name
+ * 
+ * @param name array of all name form env
+ * @return int return size of most length name
+ */
+int	get_maxlength_env(char **name)
+{
+	int	i;
+	int	len;
+
+	i = -1;
+	len = 0;
+	while (name[++i])
+	{
+		if ((int)ft_strlen(name[i]) > len)
+			len = ft_strlen(name[i]);
+	}
+	return (len);
 }
