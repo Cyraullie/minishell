b38e02c50b3ceed8dd2b45d40afd9c37af3437de
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-29 11:52:26 +0100

    adapt signal to use variable global

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,11 +6,13 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:47:36 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/01/24 14:56:04 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/29 11:46:38 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+sig_atomic_t	g_stop = 0;
 
 /**
  * @brief handle signal for SIGINT (Ctrl+C)
@@ -21,6 +23,7 @@ void	handle_sigint(int sig)
 {
 	if (sig == SIGINT)
 	{
+		g_stop = 1;
 		write(STDOUT_FILENO, "\nminishell> ", 12);
 	}
 }
@@ -48,6 +51,11 @@ void	handle_eof(char *line, char **env)
  */
 void	init_sig(void)
 {
-	signal(SIGINT, handle_sigint);
+	struct sigaction	sa;
+
+	sa.sa_handler = handle_sigint;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
 	signal(SIGQUIT, SIG_IGN);
 }
