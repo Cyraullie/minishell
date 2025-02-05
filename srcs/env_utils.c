c69466d59a5b95a4c90f8444b55022a3e0b5285e
Loic Pittet <lpittet@c2r5s2.42lausanne.ch>
2025-02-05 11:26:12 +0100

    deleted outdated todo

--- srcs/env_utils.c
+++ srcs/env_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:01:27 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/02/04 15:59:03 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:20:02 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,6 @@ char	**create_env_array(char **envp)
 	return (env);
 }
 
-//TODO adapt my_getenv to get_env_content
 /**
  * @brief Get the env content object
  * 
