1e839609959f131965b217b004faa7120a1cde12
LPittet1 <pittet82@gmail.com>
2025-02-07 13:17:33 +0100

    fixed empty line segfault

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 15:02:10 by lpittet           #+#    #+#             */
-/*   Updated: 2025/01/29 11:10:25 by lpittet          ###   ########.fr       */
+/*   Updated: 2025/02/05 13:47:27 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ char	*ft_strtrim_and_free(char *s1, char *set)
 	start = 0;
 	end = ft_strlen(s1) - 1;
 	if (end == -1)
-		return (ft_calloc(1, 1));
-	while (ft_strchr(set, s1[start]))
+		return (free(s1), ft_calloc(1, 1));
+	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	if (start > end)
-		return (ft_calloc(1, 1));
+		return (free(s1), ft_calloc(1, 1));
 	while (ft_strchr(set, s1[end]))
 		end--;
 	trimmed = ft_substr(s1, start, end - start + 1);
@@ -84,7 +84,6 @@ int	parsing(char *line, t_command **cmd, char **env)
 		return (1);
 	}
 	line = ft_strtrim_and_free(line, " ");
-	(void)env;
 	tab = mini_split(line);
 	i = 0;
 	while (tab[i])
