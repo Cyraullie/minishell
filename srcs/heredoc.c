c9bd5127b746a829dfb5664c0e3f89685a06fb83
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-02-13 14:32:05 +0100

    add todo

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 11:04:16 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/02/12 15:36:21 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/02/13 14:28:34 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	heredoc(t_command *cmd)
 	int		len;
 	char	*path;
 	char	*home;
-
+	//TODO add replace_var for $...
 	home = get_userhome();
 	path = ft_strjoin_and_free(home, HEREDOC_FILE);
 	if (!access(path, F_OK))
